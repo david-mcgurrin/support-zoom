2b2e87680922e1fadc4a1e717686ad017a4b9084
David McGurrin <david.mcgurrin@shopify.com>
2020-08-18 18:19:14 +0100

    Update variable names

--- CS2/Module-01/wordCount.cpp
+++ CS2/Module-01/wordCount.cpp
@@ -6,16 +6,16 @@ int main() {
   
   // Declare and initialise variables
   char character;
-  int counter = 0;
+  int characterCounter = 0;
   int wordCount = 0;
   int startWord = 0;
   int endWord = 0;
   int wordLength = 0;
-  int maxWord = 0;
-  int largestWord = 0;
+  int maxAmountOfCharacters = 0;
+  int longestWord = 0;
   int vowelCount = 0;
-  int largestVowelWord = 0;
-  int maxVowels = 0;
+  int mostVowelsWord = 0;
+  int maxAmountOfVowels = 0;
   bool spaceFound = false;
 
   // Opening prompt
@@ -30,48 +30,48 @@ int main() {
     // Check if a space or newline. This is where most of the logic lives as the space is used to distinguish between words
     if (character == ' ' || character  == '\n') {
 
-      // Increment the word count if counter is at least one and not a space character
-      if (counter > 0 && !spaceFound) {
+      // Increment the word count if characterCounter is at least one and not a space character
+      if (characterCounter > 0 && !spaceFound) {
         wordCount++;
       }
 
       // Once we hit a space we know we are at the end of the word
-      endWord = counter;
+      endWord = characterCounter;
 
       // Calculate the lenght of each word
       wordLength = endWord - startWord;
 
       // Get the largest word and number of characters in it
-      if (wordLength > maxWord) {
-        maxWord = wordLength;
-        largestWord = wordCount;
+      if (wordLength > maxAmountOfCharacters) {
+        maxAmountOfCharacters = wordLength;
+        longestWord = wordCount;
       }
       
       // Get the largest vowel word and number of characters in it
-      if (vowelCount > maxVowels) {
-        maxVowels = vowelCount;
-        largestVowelWord = wordCount;
+      if (vowelCount > maxAmountOfVowels) {
+        maxAmountOfVowels = vowelCount;
+        mostVowelsWord = wordCount;
       }
 
-      startWord = counter + 1; // Set the startword to be the first character after a space
-      vowelCount = 0; // Reset the vowel counter to 0
+      startWord = characterCounter + 1; // Set the startword to be the first character after a space
+      vowelCount = 0; // Reset the vowel characterCounter to 0
       spaceFound = true; // Update this to be true so that we don't continue counting spaces
     }
 
     // Case sensitve check for vowel characters
     else if (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U'
       || character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
-      vowelCount++; // Increment the vowel counter
+      vowelCount++; // Increment the vowel characterCounter
       spaceFound = false; //Reset the spaceFound boolean
     }
 
-    // We've encountered any other character. Can reset the spaceFound boolean
+    // We've encharacterCountered any other character. Can reset the spaceFound boolean
     else {
       spaceFound = false;
     }
 
-    // Increase the counter with each iteration of the loop
-    counter++;
+    // Increase the characterCounter with each iteration of the loop
+    characterCounter++;
   }
 
   // Check if at least one non-space character was entered
@@ -89,8 +89,8 @@ int main() {
       cout << "The input contains " << (wordCount) << " words.\n";
     }
 
-    cout << "Word " << largestWord << " contains the most amount of characters(" << maxWord << ").\n";
-    cout << "Word " << largestVowelWord << " contains the most amount of vowels(" << maxVowels << ").\n";
+    cout << "Word " << longestWord << " contains the most amount of characters(" << maxAmountOfCharacters << ").\n";
+    cout << "Word " << mostVowelsWord << " contains the most amount of vowels(" << maxAmountOfVowels << ").\n";
   }
 
   // Make the log clearer
