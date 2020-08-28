9e764b3e7f8ef681317b22159b9cd04bfcf4c3ad
David McGurrin <david.mcgurrin@shopify.com>
2020-08-28 21:34:23 +0100

    Translating C++ to Java

---
+++ CS2/Module-04/Question1/ScotchTape.java
@@ -0,0 +1,11 @@
+public class ScotchTape {
+  String colour;
+  int weight;
+  int length;
+
+  ScotchTape(String colour, int weight, int length) {
+    this.colour = colour;
+    this.weight = weight;
+    this.length = length;
+  }
+}
