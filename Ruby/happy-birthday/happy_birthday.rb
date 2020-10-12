9c4c5c0f7e6c9c6b2f7a626db688b1dfdc298bdb
David McGurrin <david.mcgurrin@shopify.com>
2020-10-12 14:35:03 +0100

    Part 1

---
+++ Ruby/happy-birthday/happy_birthday.rb
@@ -0,0 +1,8 @@
+print "Please enter your name: "
+
+name = gets.chomp.capitalize
+
+puts "Happy Birthday to you"
+puts "Happy Birthday to you"
+puts "Happy Birthday dear #{name}"
+puts "Happy Birthday to you."
