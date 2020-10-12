7bc2ef6b9e853d09824fa7919d81cefa6e9d8e9b
David McGurrin <david.mcgurrin@shopify.com>
2020-10-12 09:00:43 +0100

    Generate random number

--- Ruby/build-a-game/game.rb
+++ Ruby/build-a-game/game.rb
@@ -8,3 +8,8 @@ input = gets
 name = input.chomp
 
 puts "Welcome, #{name}!"
+
+# Store a random number for the player to guess.
+puts "I've got a random number between 1 and 100."
+puts "Can you guess it?"
+target = rand(100) + 1
