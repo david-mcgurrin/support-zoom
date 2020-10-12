605cd240d4c175334c8d0acacdba6bff123dcde6
David McGurrin <david.mcgurrin@shopify.com>
2020-10-12 14:47:35 +0100

    Part 2

--- Ruby/happy-birthday/happy_birthday.rb
+++ Ruby/happy-birthday/happy_birthday.rb
@@ -2,7 +2,11 @@ print "Please enter your name: "
 
 name = gets.chomp.capitalize
 
-puts "Happy Birthday to you"
-puts "Happy Birthday to you"
-puts "Happy Birthday dear #{name}"
-puts "Happy Birthday to you."
+count = 1
+
+while count <= 4
+
+  puts count == 3 ? "Happy Birthday dear #{name}" : "Happy Birthday to you"
+
+  count+=1
+end
