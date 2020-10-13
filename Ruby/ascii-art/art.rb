376c04fb24f020abc0456db09f909fd2da75396b
David McGurrin <david.mcgurrin@shopify.com>
2020-10-13 18:34:43 +0100

    Part 3 - Triangle with spaces

--- Ruby/ascii-art/art.rb
+++ Ruby/ascii-art/art.rb
@@ -2,21 +2,59 @@ print "Please enter the shape size: "
 
 shape_size = gets.chomp.to_i
 
-num_asterisks_per_row = 1
+num_asterisks_per_row = 0
 
 for i in 1..shape_size
 
-  for j in 1..shape_size
+  shape_length = shape_size * 2
 
-    if j > shape_size - num_asterisks_per_row
-      print "*"
+  for j in 1..shape_length
+
+    if j > shape_size - i && j < shape_size + i
+
+      if shape_size % 2 == 0
+        
+        if i % 2 == 1
+          if j % 2 == 0
+            print "*"
+          else
+            print " "
+          end
+        end
+  
+        if i % 2 == 0
+          if j % 2 == 1
+            print "*"
+          else
+            print " "
+          end
+        end
+
+      else
+        
+        if i % 2 == 0
+          if j % 2 == 0
+            print "*"
+          else
+            print " "
+          end
+        end
+  
+        if i % 2 == 1
+          if j % 2 == 1
+            print "*"
+          else
+            print " "
+          end
+        end
+        
+      end
+
     else
       print " "
     end
     
   end
-
-  num_asterisks_per_row += 1
 
   puts
 
