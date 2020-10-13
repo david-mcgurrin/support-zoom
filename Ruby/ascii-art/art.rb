123ccf95755240c3c28ac9237c4fd377b2e179ff
David McGurrin <david.mcgurrin@shopify.com>
2020-10-13 19:02:31 +0100

    Part 4 -Diamond

--- Ruby/ascii-art/art.rb
+++ Ruby/ascii-art/art.rb
@@ -1,18 +1,22 @@
-print "Please enter the shape size: "
+print "Please enter a number: "
 
-shape_size = gets.chomp.to_i
+number_input = gets.chomp.to_i
 
 num_asterisks_per_row = 0
 
-for i in 1..shape_size
+for i in 1..number_input
 
-  shape_length = shape_size * 2
+  if number_input % 2 == 1
+    next if i == number_input
+  end
+
+  shape_length = number_input * 2
 
   for j in 1..shape_length
 
-    if j > shape_size - i && j < shape_size + i
+    if j > number_input - i && j < number_input + i
 
-      if shape_size % 2 == 0
+      if number_input % 2 == 0
         
         if i % 2 == 1
           if j % 2 == 0
@@ -47,7 +51,68 @@ for i in 1..shape_size
             print " "
           end
         end
+
+      end
+
+    else
+      print " "
+    end
+    
+  end
+
+  puts
+
+end
+
+for i in 1..number_input
+
+  shape_length = number_input * 2
+
+  if number_input % 2 == 0
+    shape_length -= 1
+    next if i == number_input
+  end
+
+  for j in 1..shape_length
+
+    if j >= i && j <= shape_length - i + 1
+
+      if number_input % 2 == 0
         
+        if i % 2 == 1
+          if j % 2 == 0
+            print "*"
+          else
+            print " "
+          end
+        end
+  
+        if i % 2 == 0
+          if j % 2 == 1
+            print "*"
+          else
+            print " "
+          end
+        end
+
+      else
+        
+        if i % 2 == 0
+          if j % 2 == 0
+            print "*"
+          else
+            print " "
+          end
+        end
+  
+        if i % 2 == 1
+          if j % 2 == 1
+            print "*"
+          else
+            print " "
+          end
+        end
+
       end
 
     else
