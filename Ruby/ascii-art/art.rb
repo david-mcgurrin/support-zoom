ddd42a595e597c54261bc9ab179701fb46dc5178
David McGurrin <david.mcgurrin@shopify.com>
2020-10-13 16:43:56 +0100

    Part 2 - Inverted side triangle

--- Ruby/ascii-art/art.rb
+++ Ruby/ascii-art/art.rb
@@ -6,12 +6,18 @@ num_asterisks_per_row = 1
 
 for i in 1..shape_size
 
-  for j in 1..num_asterisks_per_row
-    print "*"
+  for j in 1..shape_size
+
+    if j > shape_size - num_asterisks_per_row
+      print "*"
+    else
+      print " "
+    end
+    
   end
 
   num_asterisks_per_row += 1
 
   puts
-  
+
 end
