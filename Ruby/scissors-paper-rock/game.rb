efd63765efd4426ec3259a74631aa963ac1eed81
David McGurrin <david.mcgurrin@shopify.com>
2020-10-16 14:12:53 +0100

    Part 4

--- Ruby/scissors-paper-rock/game.rb
+++ Ruby/scissors-paper-rock/game.rb
@@ -2,13 +2,15 @@ choices = ['rock', 'paper', 'scissors']
 input_values = {"rock" => 1, "paper" => 2, "scissors" => 3}
 winner_verbs = {"rock" => "crushes", "paper" => "covers", "scissors" => "cuts"}
 winner_found = false
+player1_wins = 0
+player2_wins = 0
 
 until winner_found
   puts "Rock Paper Scissors "
   print "Player 1 - Enter your selection: "
   input1 = gets.downcase.chomp
 
-  print "Player 1 - Enter your selection: "
+  print "Player 2 - Enter your selection: "
   input2 = gets.downcase.chomp
 
   if (choices.include? input1) && (choices.include? input2)
@@ -42,16 +44,26 @@ until winner_found
 
     if winner1
       puts "#{input1.capitalize} #{winner_verbs[input1]} #{input2}"
-      puts "Player 1 wins"
-      winner_found = true
+      puts
+      player1_wins += 1
     elsif winner2
       puts "#{input2.capitalize} #{winner_verbs[input2]} #{input1}"
-      puts "Player 2 wins"
-      winner_found = true
+      puts
+      player2_wins += 1
     else
       puts "Draw!"
       puts "Give it another go"
       puts
+    end
+
+    if player1_wins == 3
+      puts "Player 1 wins"
+      winner_found = true
+    end
+
+    if player2_wins == 3
+      puts "Player 2 wins"
+      winner_found = true
     end
     
   else
