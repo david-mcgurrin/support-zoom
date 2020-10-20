83d5a2b1e7540a3423c040bb056c34861c9499d1
David McGurrin <david.mcgurrin@shopify.com>
2020-10-20 11:11:24 +0100

    Part 6

--- Ruby/reviews/reviews.rb
+++ Ruby/reviews/reviews.rb
@@ -1,23 +1,21 @@
-lines = []
+def find_adjective(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1].delete(":")
+end
 
+lines = []
 File.open("reviews.txt") do |review_file|
   lines = review_file.readlines
 end
 
 relevant_lines = lines.find_all { |line| line.include? ("Truncated") }
-
 reviews = relevant_lines.reject { |line| line.include?("--") }
 
-def find_adjective(string)
-  words = string.split(" ")
-  index = words.find_index("is")
-  words[index + 1]
+adjectives = reviews.map do |review|
+  adjective = find_adjective(review)
+  "'#{adjective.capitalize}'"
 end
 
-adjectives = []
-
-reviews.each do |review|
-  adjectives << find_adjective(review)
-end
-
+puts "The critics agree, Truncates is:"
 puts adjectives
