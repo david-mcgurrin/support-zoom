529e3a61e9355440183c222ec7df0e711739b6d5
David McGurrin <david.mcgurrin@shopify.com>
2020-10-21 11:03:44 +0100

    Update code

--- Ruby/ruby-problem-solving/mastermind.rb
+++ Ruby/ruby-problem-solving/mastermind.rb
@@ -1,7 +1,11 @@
+code = (rand(6) + 1) * 1000 + (rand(6) + 1) * 100 + (rand(6) + 1) * 10 + rand(6) + 1;
+
+# code = 1234
+
+# puts code
+
 counter = 0
 found = false
-
-code = (rand(6) + 1) * 1000 + (rand(6) + 1) * 100 + (rand(6) + 1) * 10 + rand(6) + 1;
 
 until found
 
@@ -15,28 +19,42 @@ until found
   if number == code
     found = true
   end
-  
-  output_string = ""
-  output_string_small = ""
-  
+    
   number_array = number.to_s.split("")
   code_array = code.to_s.split("")
+
+  correct_place_array = []
+  incorrect_place_array = []
+
+  output_string = ""
+  output_string_small = ""
   
   number_array.each_with_index do |num, index|
 
     if num.to_i == code_array[index].to_i
-      output_string += "X"
-  
+      correct_place_array.push(num)
     else
-
       if code_array.include? num
-        output_string_small += "x"
+        incorrect_place_array.push(num)
       end
-   
     end
+
   end
 
-  puts output_string + output_string_small
+  code_array.each do |num|
+
+    if correct_place_array.include? num
+      output_string += "X"
+    
+    elsif incorrect_place_array.include? num
+      output_string_small += "x"
+    end
+
+  end
+
+  unless found
+    puts output_string + output_string_small
+  end
 
   counter += 1
 
