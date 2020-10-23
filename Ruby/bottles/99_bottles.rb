ed801bb4d14715050c984afc078c930f11f3125f
David McGurrin <david.mcgurrin@shopify.com>
2020-10-23 10:47:51 +0100

    Refactor code

--- Ruby/bottles/99_bottles.rb
+++ Ruby/bottles/99_bottles.rb
@@ -1,33 +1,29 @@
 def verse(number)
-  new_number = number -1
 
-  puts "#{number} bottle#{number > 1 ? "s" : ""} of beer on the wall,"
-  puts "#{number} bottle#{number > 1 ? "s" : ""} of beer."
-  puts "Take one down and pass it around,"
-  puts "#{number > 1 ? new_number : "no more"} bottle#{new_number> 1 ? "s" : ""} of beer on the wall."
-end
+  original_number = number
 
-def final_verse(start_num)
+  while number > 0
+    
+    new_number = number -1
+
+    puts "#{number} bottle#{number > 1 ? "s" : ""} of beer on the wall,"
+    puts "#{number} bottle#{number > 1 ? "s" : ""} of beer."
+    puts "Take one down and pass it around,"
+    puts "#{number > 1 ? new_number : "no more"} bottle#{new_number> 1 ? "s" : ""} of beer on the wall."
+    puts
+
+    number -= 1
+
+  end
+
   puts "No more bottles of beer on the wall,"
   puts "no more bottles of beer."
   puts "Go to the store and buy some more,"
-  puts "#{start_num} bottles of beer on the wall."
+  puts "#{original_number} bottles of beer on the wall."
+
 end
 
-
-print "How many bottles ya slugging today chief? "
+print "How many bottles are on the wall? "
 num_bottles = gets.chomp.to_i
 
-original_num = num_bottles
-
-while num_bottles > 0
-
-  verse(num_bottles)
-  
-  puts
-
-  final_verse(original_num) if num_bottles == 1
-
-  num_bottles -= 1
-  
-end
+verse(num_bottles)
