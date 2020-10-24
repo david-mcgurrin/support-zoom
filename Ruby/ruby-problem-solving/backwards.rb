374175cfd0d0ee16a21ae1cac2fa633efb9952b2
David McGurrin <david.mcgurrin@shopify.com>
2020-10-24 11:41:15 +0100

    Refactor code

--- Ruby/ruby-problem-solving/backwards.rb
+++ Ruby/ruby-problem-solving/backwards.rb
@@ -5,6 +5,7 @@ while name_length < 2 do
   print "Enter your full name: "
   user_name_array = gets.chomp.split(" ")
   name_length = user_name_array.length
+
 end
 
 puts "Hello #{user_name_array.last.capitalize} #{user_name_array.first.capitalize}"
