ec778955743b44d0db50ed156211095fc87270e6
David McGurrin <david.mcgurrin@shopify.com>
2020-10-24 10:10:08 +0100

    Refactor code

--- Ruby/ascii-art/art.rb
+++ Ruby/ascii-art/art.rb
@@ -1,126 +1,19 @@
-print "Please enter a number: "
+print "How big is your triangle? "
+input = gets.chomp
 
-number_input = gets.chomp.to_i
-
-num_asterisks_per_row = 0
-
-for i in 1..number_input
-
-  if number_input % 2 == 1
-    next if i == number_input
-  end
-
-  shape_length = number_input * 2
-
-  for j in 1..shape_length
-
-    if j > number_input - i && j < number_input + i
-
-      if number_input % 2 == 0
-        
-        if i % 2 == 1
-          if j % 2 == 0
-            print "*"
-          else
-            print " "
-          end
-        end
-  
-        if i % 2 == 0
-          if j % 2 == 1
-            print "*"
-          else
-            print " "
-          end
-        end
-
-      else
-        
-        if i % 2 == 0
-          if j % 2 == 0
-            print "*"
-          else
-            print " "
-          end
-        end
-  
-        if i % 2 == 1
-          if j % 2 == 1
-            print "*"
-          else
-            print " "
-          end
-        end
-
-      end
-
-    else
-      print " "
-    end
-    
-  end
-
-  puts
-
+until input.to_i.to_s == input
+  print "Try again: "
+  input = gets.chomp
 end
 
-for i in 1..number_input
+input = input.to_i
 
-  shape_length = number_input * 2
+for i in 1..input
+  print " " * (input - i)
+  puts "* " * i
+end
 
-  if number_input % 2 == 0
-    shape_length -= 1
-    next if i == number_input
-  end
-
-  for j in 1..shape_length
-
-    if j >= i && j <= shape_length - i + 1
-
-      if number_input % 2 == 0
-        
-        if i % 2 == 1
-          if j % 2 == 0
-            print "*"
-          else
-            print " "
-          end
-        end
-  
-        if i % 2 == 0
-          if j % 2 == 1
-            print "*"
-          else
-            print " "
-          end
-        end
-
-      else
-        
-        if i % 2 == 0
-          if j % 2 == 0
-            print "*"
-          else
-            print " "
-          end
-        end
-  
-        if i % 2 == 1
-          if j % 2 == 1
-            print "*"
-          else
-            print " "
-          end
-        end
-
-      end
-
-    else
-      print " "
-    end
-    
-  end
-
-  puts
-
+(input - 1).downto(1) do |i|
+  print " " * (input - i)
+  puts "* " * i
 end
