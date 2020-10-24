b9fcf5cb57786f95e8e43070faa21373911b895b
David McGurrin <david.mcgurrin@shopify.com>
2020-10-24 12:24:44 +0100

    Fix prices.rb

--- Ruby/prices/prices.rb
+++ Ruby/prices/prices.rb
@@ -1,33 +1,30 @@
-def do_something_with_every_item(array, operation)
-
-  if operation == "total" or operation == "refund"
-    amount = 0
+def total(prices)
+  amount = 0
+  prices.each do |price|
+    amount += price
   end
-  index = 0
-  while index < array.length
-
-    if operation == "total"
-      amount += array[index]
-    elsif operation == "refund"
-      amount -= array[index]
-    elsif operation == "show discounts"
-      amount_off = array[index] / 3.0
-      puts format("Your discount: $%.2f", amount_off)
-    end
-
-    index += 1
-  end
-
-  if operation == "total" or operation == "refund"
-    return amount
-  end
-
+  amount
 end
 
-prices = [3.99, 25.00, 8.99]
+def refund(prices)
+  amount = 0
+  prices.each do |price|
+    amount -= price
+  end
+  amount
+end
 
-puts format("%.2f", do_something_with_every_item(prices, "total"))
+def show_discounts(prices)
+  prices.each do |price|
+    amount_off = price / 3.0
+    puts format("Your discount: $%.2f", amount_off)
+  end
+end
 
-puts format("%.2f", do_something_with_every_item(prices, "refund"))
+prices = [3.99, 25.00,8.99]
 
-do_something_with_every_item(prices, "show discounts")
+puts format("%.2f", total(prices))
+
+puts format("%.2f", refund(prices))
+
+show_discounts(prices)
