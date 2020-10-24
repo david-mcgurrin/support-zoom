b9c5f1670e9f852edeee054da122da448d6d297e
David McGurrin <david.mcgurrin@shopify.com>
2020-10-24 19:56:39 +0100

    Refactor code

--- Ruby/scissors-paper-rock/game.rb
+++ Ruby/scissors-paper-rock/game.rb
@@ -1,12 +1,83 @@
+def get_winner(input1, input2)
+
+  input_values = {"rock" => 1, "paper" => 2, "scissors" => 3}
+
+  value1 = input_values[input1]
+  value2 = input_values[input2]
+
+  case value1
+  when 1
+    if value2 == 3
+      winner = input1
+      loser = input2
+    elsif value2 == 2
+      winner = input2
+      loser = input1
+    end
+  when 2
+    if value2 == 1
+      winner = input1
+      loser = input2
+    elsif value2 == 3
+      winner = input2
+      loser = input1
+    end
+  when 3
+    if value2 == 2
+      winner = input1
+      loser = input2
+    elsif value2 == 1
+      winner = input2
+      loser = input1
+    end
+  end
+
+  return "Draw" if winner == nil
+
+  winning_player = winner == input1 ? "Player 1" : "Player 2"
+
+  { "winner" => winner, "loser" => loser, "winning_player" => winning_player}
+
+end
+
+def result_ouput(result, wins)
+
+  winner_verbs = {"rock" => "crushes", "paper" => "covers", "scissors" => "cuts"}
+
+  winner = result["winner"]
+  loser = result["loser"]
+  winning_player = result["winning_player"]
+
+  puts "#{winner.capitalize} #{winner_verbs[winner]} #{loser}"
+  puts
+  
+  if winner_count(winning_player, wins).include? 3
+    puts "#{winning_player} wins"
+    true
+  end
+
+end
+
+def winner_count(winning_player, wins)
+
+  if winning_player == "Player 1"
+    wins[0] += 1
+  elsif winning_player == "Player 2"
+    wins[1] += 1
+  end
+
+  wins
+end
+
 choices = ['rock', 'paper', 'scissors']
-input_values = {"rock" => 1, "paper" => 2, "scissors" => 3}
-winner_verbs = {"rock" => "crushes", "paper" => "covers", "scissors" => "cuts"}
+
 winner_found = false
-player1_wins = 0
-player2_wins = 0
+
+wins = [0,0]
 
 until winner_found
-  puts "Rock Paper Scissors "
+  puts "Rock Paper Scissors"
+
   print "Player 1 - Enter your selection: "
   input1 = gets.downcase.chomp
 
@@ -15,61 +86,21 @@ until winner_found
 
   if (choices.include? input1) && (choices.include? input2)
 
-    value1 = input_values[input1]
-    value2 = input_values[input2]
+    result = get_winner(input1, input2)
 
-    winner1 = false
-    winner2 = false
-
-    case value1
-    when 1
-      if value2 == 3
-        winner1 = true
-      elsif value2 == 2
-        winner2 = true
-      end
-    when 2
-      if value2 == 1
-        winner1 = true
-      elsif value2 == 3
-        winner2 = true
-      end
-    when 3
-      if value2 == 2
-        winner1 = true
-      elsif value2 == 1
-        winner2 = true
-      end
-    end
-
-    if winner1
-      puts "#{input1.capitalize} #{winner_verbs[input1]} #{input2}"
-      puts
-      player1_wins += 1
-    elsif winner2
-      puts "#{input2.capitalize} #{winner_verbs[input2]} #{input1}"
-      puts
-      player2_wins += 1
+    if result["winner"]
+      winner_found = result_ouput(result, wins)
     else
-      puts "Draw!"
+      puts "Draw"
       puts "Give it another go"
       puts
     end
 
-    if player1_wins == 3
-      puts "Player 1 wins"
-      winner_found = true
-    end
-
-    if player2_wins == 3
-      puts "Player 2 wins"
-      winner_found = true
-    end
-    
   else
 
     puts "Oi, no cheating! Only Rock, Paper or Scissors allowed."
     puts "Give it another go"
     puts
+
   end
 end
