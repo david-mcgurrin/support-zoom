c708b29e2f23ad6ff606f52dfdacc3a44efa6ffb
David McGurrin <david.mcgurrin@shopify.com>
2020-10-24 18:29:56 +0100

    Refactor code

--- Ruby/scissors-paper-rock/game.rb
+++ Ruby/scissors-paper-rock/game.rb
@@ -1,8 +1,53 @@
-choices = ['rock', 'paper', 'scissors']
-input_values = {"rock" => 1, "paper" => 2, "scissors" => 3}
-winner_verbs = {"rock" => "crushes", "paper" => "covers", "scissors" => "cuts"}
+def get_winner(input1, input2)
 
-puts "Rock Paper Scissors "
+  input_values = {"rock" => 1, "paper" => 2, "scissors" => 3}
+
+  value1 = input_values[input1]
+  value2 = input_values[input2]
+
+  case value1
+  when 1
+    if value2 == 3
+      winner = input1
+      loser = input2
+    elsif value2 == 2
+      winner = input2
+      loser = input1
+    end
+  when 2
+    if value2 == 1
+      winner = input1
+      loser = input2
+    elsif value2 == 3
+      winner = input2
+      loser = input1
+    end
+  when 3
+    if value2 == 2
+      winner = input1
+      loser = input2
+    elsif value2 == 1
+      winner = input2
+      loser = input1
+    end
+  end
+
+  [winner, loser]
+
+end
+
+def result_ouput(result)
+
+  winner_verbs = {"rock" => "crushes", "paper" => "covers", "scissors" => "cuts"}
+
+  puts "#{result.first.capitalize} #{winner_verbs[result.first]} #{result.last}"
+
+end
+
+choices = ['rock', 'paper', 'scissors']
+
+puts "Rock Paper Scissors"
+
 print "Player 1 - Enter your selection: "
 input1 = gets.downcase.chomp
 
@@ -11,40 +56,9 @@ input2 = gets.downcase.chomp
 
 if (choices.include? input1) && (choices.include? input2)
 
-  value1 = input_values[input1]
-  value2 = input_values[input2]
+  result = get_winner(input1, input2)
 
-  winner1 = false
-  winner2 = false
-
-  case value1
-  when 1
-    if value2 == 3
-      winner1 = true
-    elsif value2 == 2
-      winner2 = true
-    end
-  when 2
-    if value2 == 1
-      winner1 = true
-    elsif value2 == 3
-      winner2 = true
-    end
-  when 3
-    if value2 == 2
-      winner1 = true
-    elsif value2 == 1
-      winner2 = true
-    end
-  end
-
-  if winner1
-    puts "#{input1.capitalize} #{winner_verbs[input1]} #{input2}"
-  elsif winner2
-    puts "#{input2.capitalize} #{winner_verbs[input2]} #{input1}"
-  else
-    puts "Draw!"
-  end
+  result_ouput(result)
   
 else
 
