ae1d2d6bbf2f4d4d493ccc0704ad47befdca0cc0
David McGurrin <david.mcgurrin@shopify.com>
2020-10-24 09:55:35 +0100

    Refactor code

--- Ruby/ascii-art/art.rb
+++ Ruby/ascii-art/art.rb
@@ -1,23 +1,16 @@
-print "Please enter the shape size: "
+print "How big is your triangle? "
+input = gets.chomp
 
-shape_size = gets.chomp.to_i
+until input.to_i.to_s == input
+  print "Try again: "
+  input = gets.chomp
+end
 
-num_asterisks_per_row = 1
+input = input.to_i
 
-for i in 1..shape_size
-
-  for j in 1..shape_size
-
-    if j > shape_size - num_asterisks_per_row
-      print "*"
-    else
-      print " "
-    end
-    
-  end
-
-  num_asterisks_per_row += 1
-
-  puts
-
+input.downto(1) do |i|
+  num_spaces = i - 1
+  num_asterisks = input - i + 1
+  
+  puts " " * num_spaces + "*" * num_asterisks
 end
