746fac8faeddcbc8b977be2498ca6a51cc38ac72
david-mcgurrin <david.mcgurrin@shopify.com>
2020-10-24 19:27:51 +0100

    Fix typo

--- Ruby/scissors-paper-rock/game.rb
+++ Ruby/scissors-paper-rock/game.rb
@@ -58,7 +58,7 @@ puts "Rock Paper Scissors"
 print "Player 1 - Enter your selection: "
 input1 = gets.downcase.chomp
 
-print "Player 1 - Enter your selection: "
+print "Player 2 - Enter your selection: "
 input2 = gets.downcase.chomp
 
 if (choices.include? input1) && (choices.include? input2)
