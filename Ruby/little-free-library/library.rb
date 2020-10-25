9ca1a2d34485c4e60a1449a90d506c12fe57c49c
David McGurrin <david.mcgurrin@shopify.com>
2020-10-25 19:32:54 +0000

    Add and return the correct books

--- Ruby/little-free-library/library.rb
+++ Ruby/little-free-library/library.rb
@@ -1,17 +1,50 @@
-library = []
-user_books = []
+def create_libary(max_books)
+  library = []
 
-count = 1
+  max_books.times do |index|
+
+    book = {"Book #{index + 1}" => rand(200)}
+    library << book
+  
+  end
+
+  library
+end
+
+def display_books(library)
+
+  library.each_with_index do |book, index|
+    book.each { |key| puts "#{key[0]} (#{key[1]} pages)" }
+  end
+
+end
+
+def take_book(library, book_choice)
+  rup = ""
+
+  library.each_with_index do |book, index|
+    book.each do |key|
+      if key[0] == book_choice
+        rup = book
+        library.delete(book)
+      end
+    end
+  end
+
+  rup
+
+end
+
+def return_book(library, book_choice)
+  library << book_choice
+
+  book_choice
+end
 
 max_books = 18
 
-while (count <= max_books)
-
-  book = {"Book #{count}" => rand(200)}
-  library.push(book)
-  count += 1
-
-end
+library = create_libary(max_books)
+user_books = []
 
 choice = 0
 
@@ -20,52 +53,65 @@ until choice == 4
   puts "1 - view the library"
   puts "2 - take a book"
   puts "3 - return a book"
+  puts "4 - exit"
   print "Enter your choice: "
 
   choice = gets.chomp.to_i
 
   puts
 
-  if choice == 1
+  case choice
+  when 1
 
-    library.each_with_index do |book, index|
-      book.each { |key| puts "#{index + 1}: #{key[0]} (#{key[1]} pages)" }
-    end
+    display_books(library)
 
-  elsif choice == 2
+  when 2
 
     print "What book would you like to take? "
 
-    book_choice = gets.chomp.to_i
+    book_choice = gets.chomp
 
-    get_book = library[book_choice]
+    user_books << take_book(library, book_choice)
 
-    library.delete_at(book_choice)
-
-    user_books.push(get_book)
-
-
-  elsif choice == 3
+  when 3
 
     print "What book would you like to return? "
 
-    book_choice = gets.chomp.to_i
+    book_choice = gets.chomp
 
-    get_book = user_books[book_choice]
+    book_found = false
 
-    user_books.delete_at(book_choice)
+    book_to_return = {}
 
-    library.push(get_book)
+    user_books.each do |book|
+      book.each do |key|
 
+        if key[0] == book_choice
+          book_found = true
+          book_to_return = book
+        end
+
+      end
+    end
+
+    unless book_found
+      puts "You can only return a book you have!"
+      print "What book would you like to return? "
+
+      book_choice = gets.chomp
+    end
+
+    user_books.delete(return_book(library, book_to_return))
+    
   end
 
   puts
 end
 
 puts "Users' books"
-puts user_books
+print user_books
 
 puts
 
 puts "Library"
-puts library
+print library
