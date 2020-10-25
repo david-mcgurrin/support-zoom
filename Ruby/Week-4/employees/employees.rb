1b8f91069b535cd2d779784edcabbd43f25471f4
David McGurrin <david.mcgurrin@shopify.com>
2020-10-25 20:23:54 +0000

    Create subclasses

--- Ruby/Week-4/employees/employees.rb
+++ Ruby/Week-4/employees/employees.rb
@@ -40,7 +40,41 @@ class SalariedEmployee < Employee
 
 end
 
+
+class HourlyEmployee < Employee
+
+  attr_reader :hourly_wage, :hours_per_week
+
+  def hourly_wage=(hourly_wage)
+    if hourly_wage < 0
+      raise "A wage of #{hourly_wage} isn't valid!"
+    end
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week=(hours_per_week)
+    if hours_per_week < 0
+      raise "Hours per week of #{hours_per_week} isn't valid!"
+    end
+    @hours_per_week = hours_per_week
+  end
+
+  def print_pay_stub
+    print_name
+    pay_for_period = hourly_wage * hours_per_week * 2
+    formatted_pay = format("$%.2f", pay_for_period)
+    puts "Pay This Period: #{formatted_pay}"
+  end
+
+end 
+
 salaried_employee = SalariedEmployee.new
 salaried_employee.name = "Jane Doe"
 salaried_employee.salary = 50000
 salaried_employee.print_pay_stub
+
+hourly_employee = HourlyEmployee.new
+hourly_employee.name = "John Smith"
+hourly_employee.hourly_wage = 14.97
+hourly_employee.hours_per_week = 30
+hourly_employee.print_pay_stub
