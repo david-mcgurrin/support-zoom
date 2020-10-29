d69384d8e3f9eb704feb0a687ae7e4d13745ae5a
David McGurrin <david.mcgurrin@shopify.com>
2020-10-29 09:46:56 +0000

    Call method and pass in blocks

--- Ruby/Week-4/multiples/multiples.rb
+++ Ruby/Week-4/multiples/multiples.rb
@@ -4,11 +4,24 @@ def customized_list_of_multiples(multiples_of, size)
     (index + 1) * multiples_of
   end
     
-  if block_given?
-    yield (list)
-  end
-
+  list = yield (list) if block_given?
+  
   print list
   puts
+
+end
+
+puts "Method call with no block"
+customized_list_of_multiples(5, 5)
+
+puts "\nMethod call with block which doubles the original numbers"
+customized_list_of_multiples(5, 5) { |list| list.map { |x| x += x } }
+
+puts "\nMethod call with block which squares the original numbers"
+customized_list_of_multiples(5,5) do |list|
+  
+  list.each_with_index do |num, index|
+    list[index] = num * num
+  end
 
 end
