41292bf710045f03ac652a388093629abd41b128
David McGurrin <david.mcgurrin@shopify.com>
2020-10-30 11:28:24 +0000

    Use constant for hash

--- Ruby/Week-5/comparable-and-enumerable/steak.rb
+++ Ruby/Week-5/comparable-and-enumerable/steak.rb
@@ -1,9 +1,11 @@
 class Steak
 
+  GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
+
   attr_accessor :grade
 
   def > (other)
-    GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
+    
     GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
   end
 
