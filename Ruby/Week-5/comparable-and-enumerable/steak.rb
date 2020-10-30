c18c3e86ea539f453392f5c238fef04d284125d8
David McGurrin <david.mcgurrin@shopify.com>
2020-10-30 11:38:23 +0000

    Fix comparrison operator

--- Ruby/Week-5/comparable-and-enumerable/steak.rb
+++ Ruby/Week-5/comparable-and-enumerable/steak.rb
@@ -1,11 +1,13 @@
 class Steak
 
+  include Comparable
+
   GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1}
 
   attr_accessor :grade
 
   def <=> (other)
-    if GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
+    if GRADE_SCORES[grade] < GRADE_SCORES[other.grade]
       return -1
     elsif GRADE_SCORES[grade] == GRADE_SCORES[other.grade]
       return 0
