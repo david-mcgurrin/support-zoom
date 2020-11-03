abeedb384814fff0d4b4664085eae2a2151e0203
David McGurrin <david.mcgurrin@shopify.com>
2020-11-03 16:53:21 +0000

    Call method and pass in blocks

--- Ruby/Week-6/practice-makes-perfect/practice.rb
+++ Ruby/Week-6/practice-makes-perfect/practice.rb
@@ -16,7 +16,7 @@ class Practice
       effectiveness = yield (study_session) if block_given?
 
       study_satisfaction << effectiveness
-
+      
       avg = study_satisfaction.reduce(:+) / study_satisfaction.size.to_f
 
       study_complete = true if  avg > 3 && study_session > 3
@@ -32,5 +32,36 @@ class Practice
     puts "Study over"
 
   end
+
+end
+
+Practice.simulate_studying("Irish") do |session|
+
+  effectiveness = rand(5) + 1
+
+  puts "  Study session #{session + 1} gets a rating of #{effectiveness}" 
+  
+  effectiveness
+
+end
+
+puts
+
+Practice.simulate_studying("spelling") do |session|
+
+  effectiveness = session
+
+  puts "  me rite gud" if effectiveness == 0 
+
+  puts "  me writng is gudder" if effectiveness == 1
+
+  puts "  me writing is bettr" if effectiveness == 2
+
+  if effectiveness  == 3
+    puts "  My writing has improved signifcantly"
+    effectiveness += 20
+  end
+
+  effectiveness
 
 end
