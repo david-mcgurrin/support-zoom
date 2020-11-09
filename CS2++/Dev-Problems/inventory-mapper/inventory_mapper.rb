574db6de9226ab361115316fb125ab0b159672fd
David McGurrin <david.mcgurrin@shopify.com>
2020-11-09 10:18:40 +0000

    Refactor code

--- CS2++/Dev-Problems/inventory-mapper/inventory_mapper.rb
+++ CS2++/Dev-Problems/inventory-mapper/inventory_mapper.rb
@@ -1,27 +1,30 @@
 class InventoryMapper
 
-  attr_accessor :input, :inventory_hash
+  attr_accessor :input, :letter_count, :inventory_hash
 
   def initialize(input)
     @input = input.split("")
+    @letter_count = {}
     @inventory_hash = {"Shelf"=> 0, "Stool"=> 0, "Table"=> 0}
+  end
+
+  def count_letters
+    @input.each do |x|
+      @letter_count[x] ? @letter_count[x] += 1 : @letter_count[x] = 1
+    end
   end
 
   def map_input_to_items
 
-    letter_count = {}
+    self.count_letters
 
-    @input.each do |x|
-      letter_count[x] ? letter_count[x] += 1 : letter_count[x] = 1
-    end
+    @letter_count.each do |k, v|
 
-    letter_count.each do |k, v|
-    
       @inventory_hash["Shelf"] = v if k == "a"
 
-      @inventory_hash["Stool"] = letter_count["c"] / 3 > v ? v : letter_count["c"] / 3 if k == "b"
+      @inventory_hash["Stool"] = @letter_count["c"] / 3 > v ? v : @letter_count["c"] / 3 if k == "b"
 
-      @inventory_hash["Table"] = letter_count["e"] / 4 > v ? v : letter_count["e"] / 4 if k == "d"
+      @inventory_hash["Table"] = @letter_count["e"] / 4 > v ? v : @letter_count["e"] / 4 if k == "d"
 
     end
 
@@ -31,7 +34,7 @@ class InventoryMapper
 
 end
 
-product_ids = "eebeedebaceeceedeceacee"
+product_ids = "abbbbbbbbbbbccc"
 inventory_mapper = InventoryMapper.new(product_ids)
 
 puts inventory_mapper.map_input_to_items
