af6e4c9a69b834ea3e75392b79a9827a476219bf
David McGurrin <david.mcgurrin@shopify.com>
2020-11-10 21:07:43 +0000

    Add Gutenburg text and test

--- Ruby/Week-7/textogram/textogram.rb
+++ Ruby/Week-7/textogram/textogram.rb
@@ -1,6 +1,6 @@
 class Textogram
 
-  attr_accessor :sentence, :case_sensitive, :allow_special_characters, :allow_spaces,  :character_count_hash, :output_string
+  attr_accessor :sentence, :case_sensitive, :allow_special_characters, :allow_spaces,  :character_count_hash, :word_count_hash, :output_string
 
   def initialize(sentence, case_sensitive = true, allow_special_characters = true, allow_spaces = true)
     @sentence = sentence
@@ -8,6 +8,7 @@ class Textogram
     @sentence.gsub!(/[^0-9A-Za-z\s]/, '') unless allow_special_characters
     @allow_spaces = allow_spaces
     @character_count_hash = {}
+    @word_count_hash = {}
     @output_string = ""
   end
 
@@ -20,36 +21,63 @@ class Textogram
     @character_count_hash = Hash[@character_count_hash.sort]
   end
 
+  def word_count
+    @sentence.split(" ").each do |word|
+      next if word == " " unless @allow_spaces
+      @word_count_hash[word] ? @word_count_hash[word] += 1 : @word_count_hash[word] = 1
+    end
+
+  end
 
   def to_s
-    @character_count_hash.each { |k, v| @output_string += "#{k} " + "*" * v + "\n" }
+    output_hash = @character_count_hash.empty? ? @word_count_hash : @character_count_hash
+
+    output_hash.each { |k, v| @output_string += "#{k} " + "*" * v + "\n" }
 
     @output_string
   end
 
 end
 
-
 textogram = Textogram.new("Hello world!", true, true, true)
 
 textogram.character_count
 
-puts "Textogram 1:"
+puts "Textogram 1 - Basic example:"
 puts textogram.to_s
-puts 
+puts
 
 new_textogram = Textogram.new("", true, true, true)
 
 new_textogram.character_count
 
-puts "Textogram 2:"
+puts "Textogram 2 - Nothing example:"
 puts new_textogram.to_s # outputs nothing
-puts 
+puts
 
 longer_sentence = Textogram.new("Hello hello; goodbye. The end!!", false, false, false)
 
 longer_sentence.character_count
 
-puts "Textogram 3:"
+puts "Textogram 3 - Longer sentence example:"
 puts longer_sentence.to_s
+puts
+
+longer_sentence = Textogram.new("Hello hello; goodbye. The end!!", false, false, false)
+
+longer_sentence.word_count
+
+puts "Textogram 4 - Word count exmaple"
+puts longer_sentence.to_s
+puts
+
+# The first 5 pages of Book I of Homer's Iliad
+input = File.read("iliad.txt").strip
+
+iliad_textogram = Textogram.new(input, true, false, false)
+
+iliad_textogram.word_count
+
+puts "Textogram 5 - Gutenburg book example:"
+puts iliad_textogram.to_s
 puts
