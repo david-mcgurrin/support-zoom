5fea2cbde38ac10f419e8927b602b2897b4646ca
David McGurrin <david.mcgurrin@shopify.com>
2020-11-11 09:41:47 +0000

    Add additional methods

--- Ruby/Week-7/treasure-hunt/treasure_hunt.rb
+++ Ruby/Week-7/treasure-hunt/treasure_hunt.rb
@@ -1,65 +1,71 @@
 class TreasureMap
 
-  attr_accessor :map_hash, :co_arr
+  attr_accessor :map_hash, :coordinates, :new_hash
 
   def initialize
     @map_hash = {}
+    @map_size = []
+    @x_coordinates = []
+    @y_coordinates = []
   end
 
   def add_item_to_map(arr, item)
-  
     @map_hash[arr] = item
-    @co_arr = []
+  end
+
+
+  def remove_item_from_map(key)
+    @map_hash.delete(key)
+  end
+
+
+  def get_coordinates
+  
+    @map_hash.each_key do |key|
+      @x_coordinates << key.first
+      @y_coordinates << key.last
+    end
+  
+  end
+
+  def find_row_and_column_lengths
+
+    @left = @x_coordinates.min.abs
+    @bottom = @y_coordinates.min.abs
+
+    @map_size = [(@x_coordinates.max - @x_coordinates.min).abs + 1, (@y_coordinates.min - @y_coordinates.max).abs]
 
   end
 
-  def find_rows_and_cols
+  def translate_map
 
-    left = @map_hash.first[0][0]
-    right = @map_hash.first[0][0]
-
-    top = @map_hash.first[0][1]
-    bottom = @map_hash.first[0][1]
-
-    #puts left, right, top, bottom
-
-    @map_hash.each_key do |key|
-      
-      if key[0] < left
-        left = key[0]
-      end
-
-      if key[0] > right
-        right  = key[0]
-      end
-
-      if key[1] > top
-        top = key[1]
-      end
-
-      if key[1] < bottom
-        bottom = key[1]
-      end
-
+    @new_hash = @map_hash.each_key do |x|
+      x[0] += @left
+      x[1] += @bottom
     end
-
-    #puts left, right, top, bottom
-
-    @co_arr = [(right - left).abs, (bottom - top).abs]
-
+  
   end
 
   def to_s
+   
+    @map_size[1].downto(0) do |y|
+      @map_size[0].times do |x|
+        
+        coordinates_found = false
 
-    @co_arr[1].times do
-      @co_arr[0].times do
-        print "."
+        @new_hash.each do |k, v|
+          if k == [x, y]
+            print v == "" ? "." : v
+            coordinates_found = true
+          end
+        end
+
+        print "." unless coordinates_found
+        
       end
       puts
     end
 
-
-    puts @map_hash
   end
 
 end
@@ -68,9 +74,17 @@ tm = TreasureMap.new
 
 tm.add_item_to_map([0,1], "F")
 tm.add_item_to_map([11,0], "F")
-tm.add_item_to_map([6,-2], "F")
-tm.add_item_to_map([-1,-5], "F")
+tm.add_item_to_map([6,-2], "X")
+tm.add_item_to_map([-1,-5], "W")
 
-tm.find_rows_and_cols
+tm.add_item_to_map([3, -3], "O")
+
+tm.remove_item_from_map([3, -3])
+
+tm.get_coordinates
+
+tm.find_row_and_column_lengths
+
+tm.translate_map
 
 tm.to_s
