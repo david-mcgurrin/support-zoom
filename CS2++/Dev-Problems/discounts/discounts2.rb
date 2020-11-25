f51541e1eadc03defa64d7b371a25db37a4df759
David McGurrin <david.mcgurrin@shopify.com>
2020-11-25 11:21:56 +0000

    Refactored solution

--- CS2++/Dev-Problems/discounts/discounts2.rb
+++ CS2++/Dev-Problems/discounts/discounts2.rb
@@ -4,101 +4,87 @@
 # [ ['grapes', 3], ['apples', 5], ['peaches', 2] ] # output => 36
 # [ ['peaches', 7], ['grapes', 7], ['apples', 7] ] # output => 85.8
 
-INPUT = [ ['grapes', 2], ['apples', 2], ['peaches', 1] ].freeze
+INPUT = [ ['grapes', 7], ['apples', 7], ['peaches', 7] ].freeze
 
-class Grapes
+class Fruit
   attr_accessor :price, :quantity
 
-  def initialize(quantity)
-    @price = 500
-    @x = 2
-    @y = 1
+  def initialize(price, quantity)
+    @price = price
     @quantity = quantity
   end
 
-  # def buy_x_get_y
-  #   @quantity /= 2
-  # end
-
-  def item_price
-    @price * (@quantity/2 +@quantity % 2)
-  end
-end
-
-class Apples
-  attr_accessor :price, :quantity
-
-  def initialize(quantity)
-    @price = 300
-    @percentage = 20 / 100
-    @quantity = quantity
-  end
-
-  # def percentage_dicount
-  #   @price -= (@price * @percentage)
-  # end
-
-  def item_price
-    @price -= (@price * 0.2)
+  def calculate_price
     @price * @quantity
   end
+
 end
 
-class Peaches
-  attr_accessor :price, :quantity
+class Grapes < Fruit
 
   def initialize(quantity)
-    @quantity = quantity
-    @price = 700
+    super(500, quantity)
+    @spend_threshold = 2
   end
 
-  def item_price
-    @price * @quantity
+  def calculate_price
+    @quantity = (@quantity/@spend_threshold + @quantity % @spend_threshold)
+    super
   end
 end
 
-class PriceCalculator
-  attr_accessor :total, :fruit
+class Apples < Fruit
+
+  def initialize(quantity)
+    super(300, quantity)
+    @percentage = 0.2
+  end
+
+  def calculate_price
+    @price -= (@price * @percentage)
+    super
+  end
+end
+
+class Peaches < Fruit
+
+  def initialize(quantity)
+    super(700, quantity)
+  end
+
+end
+
+class Cart
+  attr_accessor :total, :line_items
 
   def initialize
     @total = 0
-    @fruit = []
+    @line_items = []
   end
 
-  def products_setup(products)
-
+  def add_products(products)
     products.each do |product|
-
       case product.first
-
       when 'grapes'
-        @fruit << Grapes.new(product.last)
+        @line_items << Grapes.new(product.last)
       when 'apples'
-        @fruit << Apples.new(product.last)
+        @line_items << Apples.new(product.last)
       when 'peaches'
-        @fruit << Peaches.new(product.last)
+        @line_items << Peaches.new(product.last)
       end
-
-      
     end
-    puts @fruit
-    @fruit
-
+    @line_items
   end
 
   def calculate_total
-
-    @fruit.each do |fruit|
-      @total += fruit.item_price
-
+    @line_items.each do |line_item|
+      @total += line_item.calculate_price
     end
-  
     @total/100
   end
 
 end
 
-price_calculator = PriceCalculator.new
-price_calculator.products_setup(INPUT)
-
-puts price_calculator.calculate_total
+cart = Cart.new
+cart.add_products(INPUT)
+puts cart.calculate_total
