e764d5c6a841efcd5ec703197cd8e084d8ef1ef3
David McGurrin <david.mcgurrin@shopify.com>
2020-12-08 22:03:22 +0000

    Refactor into classes

--- CS2++/Dev-Problems/search-engine/search_engine.rb
+++ CS2++/Dev-Problems/search-engine/search_engine.rb
@@ -1,53 +1,87 @@
-product_listing = []
-File.open('products_small.csv', 'r').each_with_index do |line, index|
-  next if index == 0
-  line_array = line.split(",")
-  product_listing << {id:  line_array[0].to_i, name: line_array[1].downcase  }
-end
+class ProductListing
 
-inverted_index = {}
-product_listing.each do |product|
-  product[:name].split.each do |token|
-    inverted_index[token] ||= []
-    inverted_index[token] << product[:id]
+  def initialize(file)
+    @file = file
+    @products = []
   end
+
+  def get_products
+    File.open(@file, 'r').each do |row|
+      row_items = row.split(",")
+      @products << { id:  row_items[0].to_i, name: row_items[1].downcase  }
+    end
+    
+    @products
+  end
+
 end
 
-def list_duplicates(array)
-  duplicates = array.select { |e| array.count(e) > 1 }
-  duplicates.uniq
+class SearchEngine
+
+  def initialize(products)
+    @products = products
+    @inverted_index = {}
+    @matches = []
+  end
+
+
+  def find(search_terms, operator = 'or')
+    get_matches(search_terms)
+
+    case operator
+    when 'and'
+      duplicates = list_duplicates(@matches.flatten)
+      loop_items(duplicates)
+
+    when 'or'
+      @matches.each do |match|
+        loop_items(match)
+      end
+    end
+
+  end
+
+
+  private
+
+  def get_matches(search_terms)
+    @inverted_index = inverted_index
+    @search_terms = search_terms.split(" ")
+
+    @search_terms.each { |i| @matches << @inverted_index[i] }
+
+    @matches
+  end
+
+  def inverted_index
+    @products.each do |product|
+      product[:name].split.each do |token|
+        @inverted_index[token] ||= []
+        @inverted_index[token] << product[:id]
+      end
+    end
+    @inverted_index
+  end
+
+  def list_duplicates(array)
+    duplicates = array.select { |e| array.count(e) > 1 }
+    duplicates.uniq
+  end
+
+  def loop_items(items)
+    items.each do |item|
+      puts @products[item][:name]
+    end
+  end
+
 end
 
-# puts inverted_index["steel"]
-# => [1, 2]
 
-# puts inverted_index["steel"] & inverted_index["bag"]
-# => [1]
+products = ProductListing.new('products_small.csv').get_products
+search_engine = SearchEngine.new(products)
 
-print "Please enter your search: "
-input = gets.chomp.split(" ")
+# search_engine.find('keyboard')
 
-print input
-print input.length
-puts
+# search_engine.find('keyboard', 'or')
 
-matches = []
-
-input.each do |i|
-  puts i
-  matches << inverted_index[i]
-end
-
-dupes = list_duplicates(matches.flatten)
-
-dupes.each do |dup|
-  puts product_listing[dup][:name]
-end
-
-# print matches
-
-# matches.each do |match|
-#   match.each do |m|
-#     puts product_listing[m][:name]
-#   end
-# end
+search_engine.find('steel keyboard', 'and')
